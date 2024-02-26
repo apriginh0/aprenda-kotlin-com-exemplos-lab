9610ed6b994975bee2c4da9517e12c5b6aba5b9f
Diego Aprigio <57737033+apriginh0@users.noreply.github.com>
2024-02-26 20:11:26 -0300

    Update desafio.kt
    Criando uma imagem do projeto.

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,62 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+class Formacao(val nome: String, var nivel: Nivel, var conteudos: MutableList<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("Usuário ${usuario.nome} matriculado na formação $nome")
+    }
+
+    fun adicionarConteudo(conteudo: ConteudoEducacional) {
+        conteudos.add(conteudo)
+        println("Conteúdo ${conteudo.nome} adicionado à formação $nome")
+    }
+
+    fun listarConteudos() {
+        println("Conteúdos da formação $nome:")
+        for (conteudo in conteudos) {
+            println("${conteudo.nome} - Duração: ${conteudo.duracao} minutos")
+        }
+    }
+
+    fun listarInscritos() {
+        println("Inscritos na formação $nome:")
+        for (usuario in inscritos) {
+            println(usuario.nome)
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("João")
+    val usuario2 = Usuario("Maria")
+
+    val conteudo1 = ConteudoEducacional("Introdução à Programação", 90)
+    val conteudo2 = ConteudoEducacional("Estrutura de Dados", 120)
+    val conteudo3 = ConteudoEducacional("Algoritmos Avançados", 150)
+
+    val formacao1 = Formacao("Formação Java", Nivel.INTERMEDIARIO, mutableListOf())
+    formacao1.adicionarConteudo(conteudo1)
+    formacao1.adicionarConteudo(conteudo2)
+
+    val formacao2 = Formacao("Formação Kotlin", Nivel.BASICO, mutableListOf())
+    formacao2.adicionarConteudo(conteudo1)
+    formacao2.adicionarConteudo(conteudo3)
+
+    formacao1.matricular(usuario1)
+    formacao1.matricular(usuario2)
+    formacao2.matricular(usuario1)
+
+    formacao1.listarConteudos()
+    formacao2.listarConteudos()
+
+    formacao1.listarInscritos()
+    formacao2.listarInscritos()
 }
